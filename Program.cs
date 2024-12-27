662ff3522490c7c0cee370ec5e982e6f729f0f42
Civer <mausan.bedolla@gmail.com>
2024-12-27 02:54:24 -0600

    Refactor Program.cs by removing unused using directives to improve code clarity and maintainability

--- Program.cs
+++ Program.cs
@@ -1,13 +1,8 @@
-﻿using System;
+﻿
 using Discord;
-using Discord.Net;
-using Discord.Commands;
 using Discord.WebSocket;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
-using System.Collections.Generic;
-using System.Text.Json;
+
 
 namespace Country_Mentioned_Bot
 {
